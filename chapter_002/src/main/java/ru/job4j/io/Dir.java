d2d47fc17040f8ca0200e384f5ad03cc269c3da8
ShamRail <rail1999g102r@mail.ru>
2020-04-29 22:54:47 +0500

    5. Валидация параметров запуска. [#246865]

--- chapter_002/src/main/java/ru/job4j/io/Dir.java
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -5,6 +5,12 @@ import java.io.File;
 public class Dir {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\79962\\Documents\\job4j_junior");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid arguments. Try again, e.g. java -jar DIR_NAME .txt");
+        }
         if (!file.exists()) {
             throw new IllegalStateException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -9,9 +9,12 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Invalid arguments! Try again, e.g. java -jar C:\\ .txt");
+        }
         Path start = Paths.get("C:\\Users\\79962\\Documents\\job4j_junior");
-        search(start, p -> p.toFile().getAbsolutePath().endsWith(".java"))
-                .forEach(p -> System.out.println(p.toAbsolutePath()));
+        search(start, p -> p.toFile().getName().endsWith(".java"))
+                .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
