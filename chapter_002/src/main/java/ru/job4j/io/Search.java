fa8ff5ccb19732dfcfefd18b80e665530e3a958f
ShamRail <rail1999g102r@mail.ru>
2020-04-29 22:45:40 +0500

    4.1. Сканирование файловой системы. [#106929]

---
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get("C:\\Users\\79962\\Documents\\job4j_junior");
+        search(start, p -> p.toFile().getAbsolutePath().endsWith(".java"))
+                .forEach(p -> System.out.println(p.toAbsolutePath()));
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
