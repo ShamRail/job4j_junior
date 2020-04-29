fbb851b1fcf6017cd6d92358b852015d6848f0db
ShamRail <rail1999g102r@mail.ru>
2020-04-29 22:10:44 +0500

    3.0. Тестирование IO [#173905]

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,23 +1,36 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void simpleTest() throws IOException {
-        String src = "./data/unavailable.csv";
-        String tgt = "./data/target.csv";
+        File src = folder.newFile("unavailable.csv");
+        File tgt = folder.newFile("target.csv");
+        Files.write(src.toPath(), List.of(
+                "200 10:56:01",
+                "500 10:57:01",
+                "400 10:58:01",
+                "200 10:59:01",
+                "500 11:01:02",
+                "200 11:02:02"
+        ));
         Analizy analizy = new Analizy();
-        analizy.unavailable(src, tgt);
+        analizy.unavailable(src.getAbsolutePath(), tgt.getAbsolutePath());
         List<String> expect = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
-        List<String> result = Files.readAllLines(Path.of(tgt));
+        List<String> result = Files.readAllLines(tgt.toPath());
         assertThat(result, is(expect));
     }
 
