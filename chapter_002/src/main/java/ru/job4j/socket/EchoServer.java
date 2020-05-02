95f27eb4edea55de8fcf429258f677442e0d230c
ShamRail <rail1999g102r@mail.ru>
2020-05-02 16:45:16 +0500

    1. Бот [#7921]

--- chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
@@ -5,6 +5,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static final String HELL0 = "Hello";
+
+    private static final String EXIT = "Exit";
+
+    private static final String DATA_PATTERN = ".+\\?msg=.+ HTTP/1.1";
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,10 +20,21 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-                    if (str.matches(".+\\?msg=Bye HTTP/1\\.1")) {
-                        server.close();
-                        break;
+                    if (str != null && str.matches(DATA_PATTERN)) {
+                        String argument = str.substring(
+                                str.lastIndexOf('=') + 1,
+                                str.lastIndexOf(' ')
+                        );
+                        String answer = HELL0.equalsIgnoreCase(argument) ? "Hello" : argument;
+                        if (EXIT.equalsIgnoreCase(answer)) {
+                            answer = "Server stopped!";
+                        }
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write(answer.getBytes());
+                        if (EXIT.equalsIgnoreCase(argument)) {
+                            server.close();
+                            break;
+                        }
                     }
                 }
             }
