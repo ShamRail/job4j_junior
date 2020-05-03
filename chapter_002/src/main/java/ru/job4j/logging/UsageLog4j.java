c1dae059acb154a933338bfb2e9b996de441047f
ShamRail <rail1999g102r@mail.ru>
2020-05-03 13:54:46 +0500

    3. Slf4j - вывод переменных. [#268852]

--- chapter_002/src/main/java/ru/job4j/logging/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/logging/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte b = 1;
+        short s = 2;
+        int i = 3;
+        long l = 4L;
+        float f = 5f;
+        double d = 6D;
+        char c = '7';
+        boolean bb = true;
+        LOG.debug("Integer types: byte = {}, short = {}, int = {}, long ={}", b, s, i, l);
+        LOG.debug("Float types: float = {}, double = {}", f, d);
+        LOG.debug("Character type: char = {}", c);
+        LOG.debug("Boolean type: boolean = {}", bb);
     }
 }
