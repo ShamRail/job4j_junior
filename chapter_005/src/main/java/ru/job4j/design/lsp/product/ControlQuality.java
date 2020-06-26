0c8316312d014beb3f416c21d39137f48213a509
ShamRail <rail1999g102r@mail.ru>
2020-06-26 19:39:09 +0500

    1. Динамическое перераспределение продуктов [#854]

--- chapter_005/src/main/java/ru/job4j/design/lsp/product/ControlQuality.java
+++ chapter_005/src/main/java/ru/job4j/design/lsp/product/ControlQuality.java
@@ -3,6 +3,7 @@ package ru.job4j.design.lsp.product;
 import ru.job4j.design.lsp.product.food.Food;
 import ru.job4j.design.lsp.product.store.Store;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ControlQuality {
@@ -19,6 +20,12 @@ public class ControlQuality {
 
     public void distribute(List<Food> foods) {
         foods.forEach(this::distribute);
+    }
+
+    public void redistribute() {
+        List<Food> allFood = new LinkedList<>();
+        stores.forEach(s -> allFood.addAll(s.clear()));
+        distribute(allFood);
     }
 
 }
